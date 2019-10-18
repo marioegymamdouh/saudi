7e170bcde44c7137e275d162347f068c49c93405
marioegymamdouh <47255781+marioegymamdouh@users.noreply.github.com>
2019-10-18 19:44:23 +0200

    buy for saudi arabia page

--- js/main.js
+++ js/main.js
@@ -4,5 +4,19 @@ $(function () {
         var fileName = $(this).val();
         //replace the "Choose a file" label
         $(this).next('.custom-file-label').html(fileName);
-    })
+    });
+
+    $('.select-beast').selectize({
+        create: true,
+        sortField: {
+            field: 'text',
+            direction: 'asc'
+        },
+        dropdownParent: 'body',
+        render: {
+            option_create: function(data, escape) {
+                return '<div class="create pr-2">إضافة: <strong>' + escape(data.input) + '</strong></div>';
+            }
+        }
+    });
 });
